dfad1d46a878862c3e6f572a9ce93f8742168443
unknown <laura.garcia-a@labinfo.is.escuelaing.edu.co>
2024-04-02 16:24:54 -0500

    funcionando en local

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/MathService.java
@@ -0,0 +1,84 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.port;
+import static  spark.Spark.get;
+
+public class MathService {
+    public static void main(String... args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+        get("primes", (req, res) ->{
+            res.type("application/json");
+            return responsePrimos(Integer.parseInt(req.queryParams("value")));
+        });
+        get("factors", (req, res) ->{
+            res.type("application/json");
+            return responseFactors(Integer.parseInt(req.queryParams("value")));
+        });
+    }
+
+    public static String responsePrimos(Integer value){
+        List<Integer> primes= primes(value);
+        return "{ \n \"operation\" : \"Primes\" ,\n \"input\" : \""+value+"\" , \n \"output\" : \""+ formatList(primes)+"\" \n}";
+    }
+
+    public static String responseFactors(Integer value){
+        List<Integer> factors= factors(value);
+        return "{ \n \"operation\" : \"Factors\" ,\n \"input\" : \""+value+"\" , \n \"output\" : \""+ formatList(factors)+"\" \n}";
+    }
+
+
+    public static String formatList(List<Integer> listNumbers){
+        String response="";
+        StringBuilder stringBuilder= new StringBuilder();
+        for (int i=0; i<listNumbers.size(); i++){
+            stringBuilder.append(listNumbers.get(i));
+            response+=String.valueOf(listNumbers.get(i));
+            System.out.println("lista "+listNumbers.get(i));
+            if(i<listNumbers.size()-1){
+                stringBuilder.append(", ");
+                response+=",";
+            }
+        }
+        return response;
+    }
+
+    public static List<Integer>  factors(Integer value){
+        List<Integer> factorsn=new ArrayList<>();
+        for (int i=1; i<=value/2; i++){
+            if(value%i==0){
+                factorsn.add(i);
+            }
+        }
+        return factorsn;
+    }
+    public static List<Integer> primes(Integer value){
+        List<Integer> numberPrimes= new ArrayList<>();
+        for ( int i=2; i<=value;i++){
+            if(divisores(i)==2){
+                numberPrimes.add(i);
+            }
+        }
+        return numberPrimes;
+    }
+
+    public static int divisores(Integer number){
+        int divisores=0;
+        for (int i=1; i<=number; i++){
+            if(number%i==0){
+                divisores+=1;
+            }
+        }
+        return divisores;
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
---
+++ src/main/java/edu/escuelaing/arem/ASE/app/ServiceProxy.java
@@ -0,0 +1,114 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.io.IOException;
+
+import static spark.Spark.port;
+import static  spark.Spark.get;
+
+public class ServiceProxy {
+
+    private static final String[] serversPrimes={"http://localhost:4567/primes?value=", "http://localhost:4567/primes?value="};
+
+    private static final String[] serversFactors={"http://localhost:4567/factors?value=", "http://localhost:4567/factors?value="};
+
+   private static int urlInstancePrimes=0;
+
+    private static int urlInstanceFactors=0;
+
+
+
+    public static void main(String... args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+        get("index", (req, res)-> getIndex());
+        get("primes", (req, res) ->{
+            res.type("application/json");
+            return getResponsePrimes(req.queryParams("value"));
+        });
+        get("factors", (req, res) ->{
+            res.type("application/json");
+            return getResponseFactors(req.queryParams("value"));
+        });
+    }
+
+    public static String getResponsePrimes(String value) throws IOException {
+        return  HttpConnectionExample.getResponse(roundRobinPrimes()+value);
+    }
+
+    public static String getResponseFactors(String value) throws IOException {
+        return HttpConnectionExample.getResponse(roundRobinFactors()+value);
+    }
+
+    public static String roundRobinPrimes(){
+        urlInstancePrimes=(urlInstancePrimes+1)%serversPrimes.length;
+        return serversPrimes[urlInstancePrimes];
+    }
+
+    public static String roundRobinFactors(){
+        urlInstanceFactors=(urlInstanceFactors+1)%serversFactors.length;
+        return serversFactors[urlInstanceFactors];
+    }
+
+
+    public static String getIndex(){
+        return "\n" +
+                "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <head>\n" +
+                "        <title>Form Example</title>\n" +
+                "        <meta charset=\"UTF-8\">\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "        <h1>Form for Primes</h1>\n" +
+                "        <form action=\"/hello\">\n" +
+                "            <label for=\"name\">value:</label><br>\n" +
+                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"100\"><br><br>\n" +
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
+                "        </form> \n" +
+                "        <div id=\"getrespmsg\"></div>\n" +
+                "\n" +
+                "        <script>\n" +
+                "            function loadGetMsg() {\n" +
+                "                let nameVar = document.getElementById(\"name\").value;\n" +
+                "                const xhttp = new XMLHttpRequest();\n" +
+                "                xhttp.onload = function() {\n" +
+                "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                "                    this.responseText;\n" +
+                "                }\n" +
+                "                xhttp.open(\"GET\", \"/primes?value=\"+nameVar);\n" +
+                "                xhttp.send();\n" +
+                "            }\n" +
+                "        </script>\n" +
+                "\n" +
+                "        <h1>Form for Factors</h1>\n" +
+                "        <form action=\"/hellod\">\n" +
+                "            <label for=\"nameu\">Value:</label><br>\n" +
+                "            <input type=\"text\" id=\"nameu\" name=\"name\" value=\"13\"><br><br>\n" +
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsge()\">\n" +
+                "        </form> \n" +
+                "        <div id=\"getrespmsge\"></div>\n" +
+                "\n" +
+                "        <script>\n" +
+                "            function loadGetMsge() {\n" +
+                "                let nameVar = document.getElementById(\"nameu\").value;\n" +
+                "                const xhttp = new XMLHttpRequest();\n" +
+                "                xhttp.onload = function() {\n" +
+                "                    document.getElementById(\"getrespmsge\").innerHTML =\n" +
+                "                    this.responseText;\n" +
+                "                }\n" +
+                "                xhttp.open(\"GET\", \"/factors?value=\"+nameVar);\n" +
+                "                xhttp.send();\n" +
+                "            }\n" +
+                "        </script>\n" +
+                "    </body>\n" +
+                "</html>";
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4568;
+    }
+}
